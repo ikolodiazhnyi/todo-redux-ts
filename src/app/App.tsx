bcfab243bdcb09b5b6a58aacf067f0a3cd6ee42e
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-08-13 15:56:15 +0300

    husky test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import TodoForm from "../features/todoForm/TodoForm";
-import TodoList from "../features/todoList/TodoList";;;;
+import TodoList from "../features/todoList/TodoList";;
 import Filter from "../features/visibilityFilter/Filter";
 
 import "./App.css";
 
 export default function App(): JSX.Element {
   return (
-         <div>
+<div>
       <TodoForm />   
         <TodoList />
       <Filter />      
