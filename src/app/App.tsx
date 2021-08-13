7e6c4aa66db3876f007a582fb24e16729061c1b6
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-08-13 15:39:52 +0300

    husky test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,7 @@ import "./App.css";
 export default function App(): JSX.Element {
   return (
          <div>
-          <TodoForm />
+          <TodoForm />   
         <TodoList />
       <Filter />
     </div>
