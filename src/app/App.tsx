c7a28e636a4b80f9abf99d195c94399cfc0941c2
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-08-13 15:33:35 +0300

    husky test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import TodoForm from "../features/todoForm/TodoForm";
-import TodoList from "../features/todoList/TodoList";;
+import TodoList from "../features/todoList/TodoList";;;;
 import Filter from "../features/visibilityFilter/Filter";
 
 import "./App.css";
