f15aeb3d0bc12c1cb2de57df3ece047dcc9f01b6
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-08-14 14:40:28 +0300

    I hope this commit will be blocked

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
 import TodoForm from "../features/todoForm/TodoForm";
-import TodoList from "../features/todoList/TodoList";;
+import TodoList from "../features/todoList/TodoList";
 import Filter from "../features/visibilityFilter/Filter";
 import "./App.css";
-
-const apps = 0;
 
 export default function App(): JSX.Element {
   return (
     <div>
-      <TodoForm />   
-        <TodoList />
-      <Filter />      
-    </div> 
+      <TodoForm />
+      <TodoList />
+      <Filter />
+    </div>
   );
 }
